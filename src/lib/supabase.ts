d43729471fb0e120af3a229e94a9bb96eb3cf3f1
Harry Moore <mharry1997@gmail.com>
2025-02-25 01:50:43 +0000

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://xqzgnvoolksvgkdkoqeg.supabase.co"; /
+const supabaseUrl = "https://xqzgnvoolksvgkdkoqeg.supabase.co";
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY; 
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
